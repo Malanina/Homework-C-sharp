05b29568ef722545bb0bd45bbc1f798452859c59
Katerina <kate.malanina85@gmail.com>
2022-10-20 05:33:19 +0300

    Выдаем максимальное из трех чисел

---
+++ work2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Clear();
+
+// // семинар 1. домашнее задание.
+
+// // Задача 4: Напишите программу, которая принимает на вход три числа и выдает максимальное из этих чисел.
+ 
+ int a = 22;
+ int b = 3;
+ int c = 9;
+
+ int max = a;
+
+ if (a > max) max = a;
+ if (b > max) max = b;
+ if (c > max) max = c;
+
+Console.Write ("max = ");
+Console.WriteLine (max); 
+
+
