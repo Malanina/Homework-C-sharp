a3d200998e5ffaa650eb217bb8a5b5b24f7e2536
Katerina <kate.malanina85@gmail.com>
2022-10-24 18:46:34 +0300

    Выводит третью цифру заданного числа

---
+++ work6/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Clear();
+
+// // семинар 2. домашнее задание.
+
+// // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.Write("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = Convert.ToString(number);
+if (str.Length > 2)
+{
+    Console.Write("Третья цифра числа" + str[2]);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
+
