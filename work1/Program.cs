085e68cec55f8d2c361ae61997a2552b05b22353
Katerina <kate.malanina85@gmail.com>
2022-10-24 19:26:02 +0300

    Исправленная задача 2

--- work1/Program.cs
+++ work1/Program.cs
@@ -4,17 +4,30 @@ Console.Clear();
 // // семинар 1. домашнее задание.
 
 // // Задача 2: Напишите программу, которая на вход принимает два числа и выдает какое число большее, а какое меньшее.
- 
- int a = -9;
- int b = -3;
 
- int max = a; 
+// int a = -9;
+// int b = -3;
 
- if (a > b) max = a;
- if (b > a) max = b;
+// int max = a;
 
-     Console.Write ("max = ");
-     Console.WriteLine (max); 
+// if (a > b) max = a;
+// if (b > a) max = b;
 
-     
+// Console.Write("max = ");
+// Console.WriteLine(max);
+
+Console.WriteLine("Вводим первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Вводим второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > a) max = b;
+
+Console.WriteLine("max = ");
+Console.WriteLine(max);
+
+
+
 
