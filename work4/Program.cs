044745239cd6bcf33dc270c30ae720d2238e3f61
Katerina <kate.malanina85@gmail.com>
2022-10-24 20:09:36 +0300

    исправлена задача 8

--- work4/Program.cs
+++ work4/Program.cs
@@ -1,4 +1,5 @@
-﻿Console.Clear();
+﻿
+Console.Clear();
 
 // // семинар 1. домашнее задание.
 
@@ -10,13 +11,15 @@ int N = Convert.ToInt32(Console.ReadLine());
 
 int i = 1;
 while (i <= N)
-
+{
     if (i % 2 == 0)
 
-      Console.Write(i + " ");
+        Console.Write(i + " ");
 
-     i++;
+    i++;
+}
 
 Console.WriteLine();
+
 
 
