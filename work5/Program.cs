d67980bcaf2097b81e1890d5f3d0b0bf1936a81a
Katerina <kate.malanina85@gmail.com>
2022-10-24 17:18:27 +0300

    Показывает вторую цифру трехзначного числа

---
+++ work5/Program.cs
@@ -0,0 +1,17 @@
+﻿Console.Clear();
+
+// // семинар 2. домашнее задание.
+
+// // Задача 10: Напишите программу, которая принимает на вход трехзначное число, а на выходе показывает вторую цифру этого числа.
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+
+// Console.WriteLine($"{number/10%10}");
+
+
+Console.Write("Введите трехзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine(number);
+
+Console.WriteLine($"{number/10%10}");
