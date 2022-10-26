d1da35c478d2182189f16ea57ad27febf502dff2
Katerina <kate.malanina85@gmail.com>
2022-10-26 20:44:54 +0300

    Выдает таблицу кубов чисел

---
+++ work10/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Clear();
+
+// // семинар 3. домашнее задание.
+
+// // Задача 21: Напишите программу, которая принимает на вход число (N) и выдает таблицу кубов чисел от 1 до N.
+
+Console.Write("Введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+for (int i = 1; i < n; i++)
+{
+    Console.Write(Math.Pow(i, 3) + ",");
+}
+Console.Write(Math.Pow(n, 3));
+
+
