e40b31215406febf0c9c877489762da24a0694d1
Katerina <kate.malanina85@gmail.com>
2022-10-26 19:38:27 +0300

    Число является палиндромом

---
+++ work8/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Clear();
+
+// // семинар 3. домашнее задание.
+
+// // Задача 19: Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+
+Console.Write("Введите пятизначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int a = number / 10000;
+int b = number % 10000 / 1000;
+int c = number % 1000 / 100;
+int d = number % 100 / 10;
+int e = number % 10;
+
+if (a == e && b == d)
+    Console.WriteLine("Число является палиндромом");
+
+else
+    Console.WriteLine("Число не является палиндромом");
