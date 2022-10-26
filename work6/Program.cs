f7198bcf0acd43555f9fa519c78dfc54414f7a42
Katerina <kate.malanina85@gmail.com>
2022-10-26 21:18:35 +0300

    Показывает третью цифру заданного числа

--- work6/Program.cs
+++ work6/Program.cs
@@ -4,15 +4,30 @@
 
 // // Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
 
+// Console.Write("Введите число: ");
+// int number = Convert.ToInt32(Console.ReadLine());
+// string str = Convert.ToString(number);
+// if (str.Length > 2)
+//{
+//    Console.Write("Третья цифра числа" + str[2]);
+//}
+// else
+//{
+//    Console.Write("Третьей цифры нет");
+//}
+
+
 Console.Write("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
-string str = Convert.ToString(number);
-if (str.Length > 2)
+int a = (number /100) % 10;
+
+if (number > 99)
 {
-    Console.Write("Третья цифра числа" + str[2]);
+Console.WriteLine("Третья цифра числа " + a);
 }
 else
 {
-    Console.Write("Третьей цифры нет");
+Console.WriteLine("Третьей цифры нет");
 }
+
 
